23887f614188d2c81728e8731c8f0d101ef385c4
grizet_j <jean.grizet@epitech.eu>
2015-09-27 19:34:22 +0200

    (Aabb2) : new Class Aabb2

---
+++ src/aabb2.js
@@ -0,0 +1,268 @@
+/**
+ * Created by grizet_j on 9/21/2015.
+ */
+
+module.exports = Aabb2;
+
+var Vector2 = require('./vector2.js');
+
+/**
+ * @class Aabb2
+ * Defines a 2-dimensional axis-aligned bounding box between a [min] and a [max] position.
+ * @constructor
+ */
+function Aabb2() {
+    /// The minimum point defining the AABB.
+    /**
+     * @property min
+     * @type {Vector2}
+     */
+    this.min = Vector2.zero();
+
+    /// The maximum point defining the AABB.
+    /**
+     * @property max
+     * @type {Vector2}
+     */
+    this.max = Vector2.zero();
+}
+
+/**
+ * @method
+ * Returns the center of this
+ */
+Aabb2.prototype.__defineGetter__("center", function() {
+    var tmp = this.min.clone();
+    tmp.add(this.max);
+    tmp.scale(0.5);
+    return tmp;
+});
+
+
+/**
+ * @static
+ * Create a new AABB as a copy of [other].
+ * @param other {Aabb2}
+ * @return {Aabb2}
+ */
+Aabb2.copy = function(other) {
+    var bb = new Aabb2();
+    bb.min.setFrom(other.min);
+    bb.max.setFrom(other.max);
+    return bb;
+};
+
+/**
+ * @static
+ * Create a new AABB with a [min] and [max].
+ * @param min {Vector2}
+ * @param max {Vector2}
+ * @returns {Aabb2}
+ */
+Aabb2.minMax = function(min,  max) {
+    var bb = new Aabb2();
+    bb.min.setFrom(min);
+    bb.max.setFrom(max);
+    return bb;
+};
+
+/**
+ * @static
+ * Create a new AABB with a [center] and [halfExtents].
+ * @param center {Vector2}
+ * @param halfExtents {Vector2}
+ * @returns {Aabb2}
+ */
+Aabb2.centerAndHalfExtents = function(center, halfExtents) {
+    var bb = new Aabb2();
+    bb.setCenterAndHalfExtents(center, halfExtents);
+    return bb;
+};
+
+/**
+ * @static
+ * Constructs [Aabb2] with a min/max [storage] that views given [buffer]
+ * starting at [offset]. [offset] has to be multiple of
+ * [Float32Array.BYTES_PER_ELEMENT].
+ * @param buffer {buffer}
+ * @param offset {number}
+ * @returns {Aabb2}
+ */
+Aabb2.fromBuffer = function(buffer, offset) {
+    var bb = new Aabb2();
+    bb.min = Vector2.fromBuffer(buffer, offset);
+    bb.max = Vector2.fromBuffer(buffer, offset + Float32Array.BYTES_PER_ELEMENT * 2);
+    return bb;
+};
+
+/**
+ * @method
+ * Set the AABB by a [center] and [halfExtents].
+ * @param center {Vector2}
+ * @param halfExtents {Vector2}
+ */
+Aabb2.prototype.setCenterAndHalfExtents = function(center, halfExtents) {
+    this.min.setFrom(center);
+    this.min.sub(halfExtents);
+    this.max.setFrom(center);
+    this.max.add(halfExtents);
+};
+
+/**
+ * @method
+ * Copy the [center] and the [halfExtends] of [this].
+ * @param center {Vector2}
+ * @param halfExtents {Vector2}
+ */
+Aabb2.prototype.copyCenterAndHalfExtents = function(center, halfExtents) {
+    center.setFrom(this.min);
+    center.add(this.max);
+    center.scale(0.5);
+    halfExtents.setFrom(this.max);
+    halfExtents.sub(this.min);
+    halfExtents.scale(0.5);
+};
+
+/**
+ * @method
+ * Copy the [min] and [max] from [other] into [this].
+ * @param other {Aabb2}
+ */
+Aabb2.prototype.copyFrom = function(other) {
+    this.min.setFrom(other.min);
+    this.max.setFrom(other.max);
+};
+
+/**
+ * @method
+ * Transform [this] by the transform [t].
+ * @param t {Matrix3}
+ * @returns {Aabb2}
+ */
+Aabb2.prototype.transform = function(t) {
+    var center = Vector2.zero();
+    var halfExtents = Vector2.zero();
+    this.copyCenterAndHalfExtents(center, halfExtents);
+    t.transform2(center);
+    t.absoluteRotate2(halfExtents);
+    this.min.setFrom(center);
+    this.min.sub(halfExtents);
+    this.max.setFrom(center);
+    this.max.add(halfExtents);
+    return this;
+};
+
+/**
+ * @method
+ * Rotate [this] by the rotation matrix [t].
+ * @param t {Matrix3}
+ * @returns {Aabb2}
+ */
+Aabb2.prototype.rotate = function(t) {
+    var center = Vector2.zero();
+    var halfExtents = Vector2.zero();
+    this.copyCenterAndHalfExtents(center, halfExtents);
+    t.absoluteRotate2(halfExtents);
+    this.min.setFrom(center);
+    this.min.sub(halfExtents);
+    this.max.setFrom(center);
+    this.max.add(halfExtents);
+    return this;
+};
+
+/**
+ * @method
+ * Create a copy of [this] that is transformed by the transform [t] and store
+ * it in [out].
+ * @param t {Matrix3}
+ * @param out {Aabb2}
+ */
+Aabb2.prototype.transformed = function(t, out) {
+    out.copyFrom(this);
+    out.transform(t);
+};
+
+/**
+ * @method
+ * Create a copy of [this] that is rotated by the rotation matrix [t] and
+ * store it in [out].
+ * @param t {Matrix3}
+ * @param out {Aabb2}
+ */
+Aabb2.prototype.rotated = function(t, out) {
+    out.copyFrom(this);
+    out.rotate(t);
+};
+
+/**
+ * @method
+ * Set the min and max of [this] so that [this] is a hull of [this] and [other].
+ * @param other
+ */
+Aabb2.prototype.hull = function(other) {
+    Vector2.min(this.min, other.min, this.min);
+    Vector2.max(this.max, other.max, this.max);
+};
+
+/**
+ * @method
+ * Set the min and max of [this] so that [this] contains [point].
+ * @param point {Vector2}
+ */
+Aabb2.prototype.hullPoint = function(point) {
+    Vector2.min(this.min, point, this.min);
+    Vector2.max(this.max, point, this.max);
+};
+
+/**
+ * @method
+ * Return if [this] contains [other].
+ * @param other {Aabb2}
+ * @returns {boolean}
+ */
+Aabb2.prototype.containsAabb2 = function(other) {
+    var otherMax = other.max;
+    var otherMin = other.min;
+
+    return (this.min.x < otherMin.x) &&
+           (this.min.y < otherMin.y) &&
+           (this.max.y > otherMax.y) &&
+           (this.max.x > otherMax.x);
+};
+
+/**
+ * @method
+ * Return if [this] contains [other].
+ * @param other {Vector2}
+ * @returns {boolean}
+ */
+Aabb2.prototype.containsVector2 = function(other) {
+    return (this.min.x < other.x) &&
+           (this.min.y < other.y) &&
+           (this.max.x > other.x) &&
+           (this.max.y > other.y);
+};
+
+/**
+ * @method
+ * Return if [this] intersects with [other].
+ * @param other {Aabb2}
+ * @returns {boolean}
+ */
+Aabb2.prototype.intersectsWithAabb2 = function(other) {
+    var otherMax = other.max;
+    var otherMin = other.min;
+
+    return (this.min.x <= otherMax.x) &&
+           (this.min.y <= otherMax.y) &&
+           (this.max.x >= otherMin.x) &&
+           (this.max.y >= otherMin.y);
+};
+
+/// Return if [this] intersects with [other].
+Aabb2.prototype.intersectsWithVector2 = function(other) {
+    return (this.min.x <= other.x) &&
+           (this.min.y <= other.y) &&
+           (this.max.x >= other.x) &&
+           (this.max.y >= other.y);
+};
---
+++ test/aabb2.js
@@ -0,0 +1,212 @@
+
+var Aabb2 = require('../src/aabb2.js');
+var Matrix2 = require('../src/matrix2.js');
+var Matrix3 = require('../src/matrix3.js');
+var Vector2 = require('../src/vector2.js');
+var TEST = require('./test_utils.js');
+
+module.exports = {
+
+    testAabb2Center: function(test) {
+        test.expect(2);
+        var aabb = Aabb2.minMax(new Vector2(1.0, 2.0), new Vector2(8.0, 16.0));
+        var center = aabb.center;
+
+        test.equals(center.x, (4.5));
+        test.equals(center.y, (9.0));
+        test.done();
+    },
+
+    testAabb2CopyCenterAndHalfExtents: function(test) {
+        test.expect(4);
+        var a1 = Aabb2.minMax(new Vector2(10.0, 20.0), new Vector2(20.0, 40.0));
+        var a2 = Aabb2.minMax(new Vector2(-10.0, -20.0), new Vector2(0.0, 0.0));
+
+        var center = Vector2.zero();
+        var halfExtents = Vector2.zero();
+
+        a1.copyCenterAndHalfExtents(center, halfExtents);
+
+        test.ok(center.almostEquals(new Vector2(15.0, 30.0)));
+        test.ok(halfExtents.almostEquals(new Vector2(5.0, 10.0)));
+
+        a2.copyCenterAndHalfExtents(center, halfExtents);
+
+        test.ok(center.almostEquals(new Vector2(-5.0, -10.0)));
+        test.ok(halfExtents.almostEquals(new Vector2(5.0, 10.0)));
+        test.done();
+    },
+
+    testAabb2CenterAndHalfExtents: function(test) {
+        test.expect(4);
+        var a1 = Aabb2.centerAndHalfExtents(new Vector2(0.0, 0.0), new Vector2(10.0, 20.0));
+        var a2 = Aabb2.centerAndHalfExtents(new Vector2(-10.0, -20.0), new Vector2(10.0, 20.0));
+
+        test.ok(a1.min.almostEquals(new Vector2(-10.0, -20.0)));
+        test.ok(a1.max.almostEquals(new Vector2(10.0, 20.0)));
+
+        test.ok(a2.min.almostEquals(new Vector2(-20.0, -40.0)));
+        test.ok(a2.max.almostEquals(new Vector2(0.0, 0.0)));
+        test.done();
+    },
+
+    testAabb2SetCenterAndHalfExtents: function(test) {
+        test.expect(4);
+        var a1 = new Aabb2();
+        var a2 = new Aabb2();
+
+        a1.setCenterAndHalfExtents(new Vector2(0.0, 0.0), new Vector2(10.0, 20.0));
+
+        test.ok(a1.min.almostEquals(new Vector2(-10.0, -20.0)));
+        test.ok(a1.max.almostEquals(new Vector2(10.0, 20.0)));
+
+        a2.setCenterAndHalfExtents(new Vector2(-10.0, -20.0), new Vector2(10.0, 20.0));
+
+        test.ok(a2.min.almostEquals(new Vector2(-20.0, -40.0)));
+        test.ok(a2.max.almostEquals(new Vector2(0.0, 0.0)));
+        test.done();
+    },
+
+    testAabb2ContainsAabb2: function(test) {
+        test.expect(5);
+        var parent = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(8.0, 8.0));
+        var child = Aabb2.minMax(new Vector2(2.0, 2.0), new Vector2(7.0, 7.0));
+        var cutting = Aabb2.minMax(new Vector2(0.0, 0.0), new Vector2(5.0, 5.0));
+        var outside = Aabb2.minMax(new Vector2(10.0, 10.0), new Vector2(20.0, 20.0));
+        var grandParent = Aabb2.minMax(new Vector2(0.0, 0.0), new Vector2(10.0, 10.0));
+
+        test.equals(parent.containsAabb2(child), true);
+        test.equals(parent.containsAabb2(parent), false);
+        test.equals(parent.containsAabb2(cutting), false);
+        test.equals(parent.containsAabb2(outside), false);
+        test.equals(parent.containsAabb2(grandParent), false);
+        test.done();
+    },
+
+    testAabb2ContainsVector2: function(test) {
+        test.expect(3);
+        var parent = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(8.0, 8.0));
+        var child = new Vector2(2.0, 2.0);
+        var cutting = new Vector2(1.0, 8.0);
+        var outside = new Vector2(-1.0, 0.0);
+
+        test.equals(parent.containsVector2(child), true);
+        test.equals(parent.containsVector2(cutting), false);
+        test.equals(parent.containsVector2(outside), false);
+        test.done();
+    },
+
+    testAabb2IntersectionAabb2: function(test) {
+        test.expect(11);
+        var parent = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(8.0, 8.0));
+        var child =  Aabb2.minMax(new Vector2(2.0, 2.0), new Vector2(7.0, 7.0));
+        var cutting =  Aabb2.minMax(new Vector2(0.0, 0.0), new Vector2(5.0, 5.0));
+        var outside =  Aabb2.minMax(new Vector2(10.0, 10.0), new Vector2(20.0, 20.0));
+        var grandParent =  Aabb2.minMax(new Vector2(0.0, 0.0), new Vector2(10.0, 10.0));
+
+        var siblingOne =  Aabb2.minMax(new Vector2(0.0, 0.0), new Vector2(3.0, 3.0));
+        var siblingTwo =  Aabb2.minMax(new Vector2(3.0, 0.0), new Vector2(6.0, 3.0));
+        var siblingThree = Aabb2.minMax(new Vector2(3.0, 3.0), new Vector2(6.0, 6.0));
+
+        test.equals(parent.intersectsWithAabb2(child), true);
+        test.equals(child.intersectsWithAabb2(parent), true);
+
+        test.equals(parent.intersectsWithAabb2(parent), true);
+
+        test.equals(parent.intersectsWithAabb2(cutting), true);
+        test.equals(cutting.intersectsWithAabb2(parent), true);
+
+        test.equals(parent.intersectsWithAabb2(outside), false);
+        test.equals(outside.intersectsWithAabb2(parent), false);
+
+        test.equals(parent.intersectsWithAabb2(grandParent), true);
+        test.equals(grandParent.intersectsWithAabb2(parent), true);
+
+        test.equals(siblingOne.intersectsWithAabb2(siblingTwo), true);
+        test.equals(siblingOne.intersectsWithAabb2(siblingThree), true);
+        test.done();
+    },
+
+    testAabb2IntersectionVector2: function(test) {
+        test.expect(3);
+        var parent = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(8.0, 8.0));
+        var child = new Vector2(2.0, 2.0);
+        var cutting = new Vector2(1.0, 8.0);
+        var outside = new Vector2(-1.0, 0.0);
+
+        test.equals(parent.intersectsWithVector2(child), true);
+        test.equals(parent.intersectsWithVector2(cutting), true);
+        test.equals(parent.intersectsWithVector2(outside), false);
+        test.done();
+    },
+
+    testAabb2Hull: function(test) {
+        test.expect(4);
+        var a = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(3.0, 4.0));
+        var b = Aabb2.minMax(new Vector2(3.0, 2.0), new Vector2(6.0, 2.0));
+
+        a.hull(b);
+
+        test.equals(a.min.x, (1.0));
+        test.equals(a.min.y, (1.0));
+        test.equals(a.max.x, (6.0));
+        test.equals(a.max.y, (4.0));
+        test.done();
+    },
+
+    testAabb2HullPoint: function(test) {
+        test.expect(8);
+        var a = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(3.0, 4.0));
+        var b = new Vector2(6.0, 2.0);
+
+        a.hullPoint(b);
+
+        test.equals(a.min.x, (1.0));
+        test.equals(a.min.y, (1.0));
+        test.equals(a.max.x, (6.0));
+        test.equals(a.max.y, (4.0));
+
+        var c = new Vector2(0.0, 1.0);
+
+        a.hullPoint(c);
+
+        test.equals(a.min.x, (0.0));
+        test.equals(a.min.y, (1.0));
+        test.equals(a.max.x, (6.0));
+        test.equals(a.max.y, (4.0));
+        test.done();
+    },
+
+    testAabb2Rotate: function(test) {
+        test.expect(6);
+        var rotation = Matrix3.rotationZ(Math.PI / 4);
+        var input = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(3.0, 3.0));
+
+        var result = input.rotate(rotation);
+
+        TEST.relativeTest(test, result.min.x, 2 - Math.sqrt(2));
+        TEST.relativeTest(test, result.min.y, 2 - Math.sqrt(2));
+        TEST.relativeTest(test, result.max.x, 2 + Math.sqrt(2));
+        TEST.relativeTest(test, result.max.y, 2 + Math.sqrt(2));
+        TEST.relativeTest(test, result.center.x, 2.0);
+        TEST.relativeTest(test, result.center.y, 2.0);
+        test.done();
+    },
+
+    testAabb2Transform: function(test) {
+        test.expect(6);
+        var rotation = Matrix3.rotationZ(Math.PI / 4);
+        var input = Aabb2.minMax(new Vector2(1.0, 1.0), new Vector2(3.0, 3.0));
+
+        var result = input.transform(rotation);
+        var newCenterY = Math.sqrt(8);
+
+        TEST.relativeTest(test, result.min.x, -Math.sqrt(2));
+        TEST.relativeTest(test, result.min.y, newCenterY - Math.sqrt(2));
+        TEST.relativeTest(test, result.max.x, Math.sqrt(2));
+        TEST.relativeTest(test, result.max.y, newCenterY + Math.sqrt(2));
+        TEST.relativeTest(test, result.center.x, 0.0);
+        TEST.relativeTest(test, result.center.y, newCenterY);
+        test.done();
+    }
+};
