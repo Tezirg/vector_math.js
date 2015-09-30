ccb54ab93db0e6d20c5042ca1fce7ce29fa9c9e7
grizet_j <jean.grizet@epitech.eu>
2015-09-30 03:34:51 +0200

    (Common) : Random to global namespace

--- src/common.js
+++ src/common.js
@@ -6,6 +6,7 @@ var SIMD = require("simd");
 function vector_math() {}
 
 vector_math.EPSILON = typeof Number.EPSILON !== undefined ? Number.EPSILON : 1e-6;
+vector_math.RANDOM = Math.random;
 vector_math.ENABLE_SIMD =  false;
 vector_math.SIMD_AVAILABLE = typeof SIMD !== undefined;
 vector_math.USE_SIMD = function() { return vector_math.ENABLE_SIMD && vector_math.SIMD_AVAILABLE };
